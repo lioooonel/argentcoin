aa422974f42eea751cb7c2cdefb5c67e2dd305d5
lioooonel <lionel11arez@gmail.com>
2024-06-13 18:28:39 -0300

    avance del main

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <link rel="icon" href="sol_logo.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="header.css">
 <link rel="stylesheet" type="text/css" href="nav.css">
 <link rel="stylesheet" type="text/css" href="index.css">
 <title>Argentcoin</title>
@@ -12,19 +13,24 @@
 </head>
 <body>
 <?php
+include 'header.php';
 include 'nav_inicial.php';
 ?>
 <main>
-    <div class="cont-img">
-        <img class="img_inicio" src="elementos/argentcoin.gif" alt="">
-    </div>
-    <div class="cuadros">
-      <div class="cuadro"></div>
-      <div class="cuadro"></div>
-      <div class="cuadro"></div>
-
-      <div class="cuadro"></div>
-    </div>
+    <div class="conten-prin">
+      <section id="info-pag">
+            <h1 class="titulo">¿QUE ES?</h1>
+            <p>jahsdioashdaoishdiawhiodahslkhwihasdw </p>
+      </section>
+  <article id="ultimos-agre">
+    <h1 class="titulo">ULTIMOS AGREGADOS</h1>
+    <section class="monedas">
+      <aside class="moneda"></aside>
+      <aside class="moneda"></aside>
+      <aside class="moneda"></aside>
+    </section>
+  </article>
+        </div>
 </main>
 </body>
 </html>
--- nav_inicial.php
+++ nav_inicial.php
@@ -1,31 +1,26 @@
 <?php
-echo'<header class="contenedor-logo">
-  <art class="cont_logo">
-    <img class="logo" src="sol_lo.png" alt="">
-  </art>
-  <h1 class="nombre-marca">ARGENTCOIN</h1>
-  <h2 class="usuario">leo</h2>
-  </header>
+echo'
   <nav>
-  <div class="diseno-diagonal">
-    <div class="triangulo"></div>
+
+  <div class="diseno-diagonal1">
+    <div class="triangulo1"></div>
   </div>
   <ul>
-    <li class="li"><a href="#">CATALOGO</a>
-      <div class="submenu">
+  <li class="li"><a href="#">CATALOGO</a>
+  <div class="submenu">
         <a class="items" href="#">Subopción 1</a>
         <a class="items" href="#">Subopción 2</a>
-      </div>
-    </li class="li">
+        </div>
+        </li class="li">
     <li class="li"><a href="#">GUARDADOS</a>
-      <div class="submenu">
+    <div class="submenu">
         <a class="items" href="#">Subopción 1</a>
         <a class="items" href="#">Subopción 2</a>
-      </div>
-    </li>
+        </div>
+        </li>
     <li class="li"><a href="#">NOSOTROS</a></li>
     <li class="li"><a href="#">CONTACTANOS</a></li>
-  </ul>
+    </ul>
 </nav>'
 
 ;
